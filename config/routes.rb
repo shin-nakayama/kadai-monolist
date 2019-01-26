903fcefcd1ac6b92b1a3a5b6caa2e4159a9d1703
tech.naka <tech.naka9@gmail.com>
2019-01-26 10:39:07 +0000

    want

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'ownerships/create'
+
+  get 'ownerships/destroy'
+
   get 'items/new'
 
   root to: 'toppages#index'
@@ -10,6 +14,7 @@ Rails.application.routes.draw do
   get 'signup', to: 'users#new'
   resources :users, only: [:show, :new, :create]
   
-  resources :items, only: [:new]
+  resources :items, only: [:show, :new]
+  resources :ownerships, only: [:create, :destroy]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
