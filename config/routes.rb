715f55454505cc7c6fb7d60287270ff7590d05fd
tech.naka <tech.naka9@gmail.com>
2019-01-26 08:50:47 +0000

    rakuten api

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'items/new'
+
   root to: 'toppages#index'
 
   get 'login', to: 'sessions#new'
@@ -7,5 +9,7 @@ Rails.application.routes.draw do
 
   get 'signup', to: 'users#new'
   resources :users, only: [:show, :new, :create]
+  
+  resources :items, only: [:new]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
